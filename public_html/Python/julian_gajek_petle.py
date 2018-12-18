2deff085a0a767fd0dc36f93b181ca5421072f37
jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2018-12-18 12:52:42 +0100

    petle

--- public_html/Python/julian_gajek_petle.py
+++ public_html/Python/julian_gajek_petle.py
@@ -23,36 +23,48 @@
 #  
 
     
-suma = 0
-def cw1():
-    
-    if ('suma' >= 75):
-        print ("suma wynosi:")
-        print ('suma')
+def zad1():
+    suma=0
+    while suma<75:
+        a=int(input("podaj sumowaną liczbę"))
+        suma+=a
+    print(suma)
+
+
+def zad2():
+    n=int(input("podaj najmniejszą liczbę dodatnią z przedziału licb całkowitych"))
+    if n>0:
+        m=int(input("podaj największą liczbę całkowitą z przedziału"))
+        if m>n:
+            for i in range(n,m+1):
+                print(" ,", i)
     else:
-        a = int(input("Podaj liczbę"))
-        'suma' + 'a'
-
-def cw2(): 
-    print ('wartość', end='')
-   
-
-def cw3():      
-        
-pass   
-
-
-def cw2():
-    for i in range()
-
-pass
-
-
-
+        print("ERROR")
+def zad3():
+    m=int(input("podaj największą liczbę całkowitą"))
+    for i in range(0,m+1):
+        print(i**2)
+def zad4_concept1():
+    for i in range(1,9):
+        for j in range(0,9):
+            if not j%2:
+                if not j%3 and not i%3:
+                    print(i,j)
+def zad4():
+    print("zad4")
+    for i in range(10,99,2):
+        if not i%3:
+            print(i)
+zad1()
+zad2()
+zad3()
+zad4()
 
 def main(args):
-    
-    
+    zad1()
+    zad2()
+    zad3()
+    zad4()
     return 0
 
 if __name__ == '__main__':
