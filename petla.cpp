241384b139923c95c0499222f5d576de4ba5e3c8
Jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2019-09-16 10:36:00 +0200

    petle

---
+++ petla.cpp
@@ -0,0 +1,22 @@
+/*
+ * petla.cpp
+ * 
+
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    for(int i=0; i <= 20; i += 2 ) {
+            cout <<"HWDPJP2GMDPL"<<endl;
+            cout <<"Hello!";
+    }
+	
+	return 0;
+}
+
