67302dac06022e673f96cd6a151d1fe9a8c7a961
jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2019-10-03 19:34:13 +0200

    choinka

--- cpp/jgajek_choinka.cpp
+++ cpp/jgajek_choinka.cpp
@@ -1,32 +1,44 @@
-/*
- * jgajek_choinka.cpp
- * 
- * Copyright 2019  <>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
 
 
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
+#include <iomanip>
+
+using namespace std;
+
+void choinka(int x, char z, char p) {
+    for (int i = 1; i <= x-1; i++) {
+        for (int k = 1 ; k <= x-i-1 ; k++) {
+            cout << " ";
+}
+            for (int j = 1; j <= 2*i-1 ; j++) {
+                cout << z;
+}
+        cout << endl;
 }
 
+    for (int pien = 1; pien <= x-2; pien++) {
+		cout << " ";
+}
+		cout << p;
+}
+
+int main() {
+    int x;
+    char znak1;
+    char znak2;
+    cout << "Podaj wysokosc choinki: ";
+    cin >> x;
+    cout<< "Podaj znak korony: ";
+    cin>>znak1;
+    cout<< "Podaj znak pniaka: ";
+    cin>>znak2;
+    choinka(x, znak1, znak2);
+    
+    
+    
+    
+    
+    
+    return 0;
+}
