66242b07c859b23154ea5c1662b105f9a13abda0
jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2019-10-09 22:26:46 +0200

    zadanie

--- cpp/funkcje_zad.cpp
+++ cpp/funkcje_zad.cpp
@@ -21,26 +21,55 @@
  * 
  */
 
-
 #include <iostream>
 
 using namespace std;
-
-int staz = 1;
-float zarobek = 1000;
-
-void drukuj() {
-    cout << "Pracujesz " << staz << "lat i zarabiasz " << zarobek << endl;
+ 
+    int staz = 1;
+    
+    float zarobek = 1000;
+    
+    int lata = 1;
+ 
+void drukuj()	{
+	
+    cout<< "Pracujesz"<< staz << "lat i zarabiasz" << zarobek << endl;
+    
 }
 
-void awans(int &staz, float &zarobek) {
+int awans()	{
+	
     staz += 1;
-    zarobek = zarobek * 1.1;
+    zarobek = zarobek * 1.1 ;
+    return staz, zarobek;
+    
 }
-
-int main(int argc, char **argv)
-{
-	awans(staz, zarobek);
+ 
+int main(int argc, char **argv)	{
+	
+    cout << "Podaj przewidywany okres pracy w latach: " << endl;
+    cin >> lata;
+ 
+    for (int i = 0; i < lata; i++)	{
     drukuj();
-	return 0;
+    awans();
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
 }
