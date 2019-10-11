054be82453d675966adbb2790fc8d0453654ff68
Jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2019-10-11 09:40:59 +0200

    piejdzpi

---
+++ public_html/phpapp/inc/functions.php
@@ -0,0 +1,10 @@
+<?php
+function get_page_content($sid) {
+	if(file_exists($id.'html'))
+		include($id.'.html');
+	else
+		include('404.html')
+}
+void get_page_content(string id)
+def get_page_content(id):
+?>
