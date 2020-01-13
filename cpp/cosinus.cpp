7223e35247be66606df1f41e10fab39af56ba23b
Jupixxxksxxx <julian.gajek@lo1.sandomierz.pl>
2020-01-13 10:36:01 +0100

    nien't

---
+++ cpp/cosinus.cpp
@@ -0,0 +1,58 @@
+/*
+ * cosinus.cpp
+ * 
+ * Copyright 2020  <>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char **argv) {
+	
+    float stopien, radian;
+    stopien = 0.0;
+    //cout << cos(90 * M_PI / 180) << endl;
+    //return 0;
+    
+    do {
+        radian = stopien * M_PI / 180;
+        cout << "cos(" << stopien << ")" << cos(radian) << endl;
+        stopien += 10.0;
+    } while (stopien <= 90.0);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	return 0;
+}
